60b0cd5dd37662254f19755bd50912b805512540
smusuvathi <smusuvathi@infoblox.com>
2022-04-18 14:12:58 +0530

    new java file

---
+++ demo/src/main/java/com/tomcatdeploy/NewApp.java
@@ -0,0 +1,13 @@
+package com.tomcatdeploy;
+
+public class NewApp {
+    
+    public static void surVar()
+    {
+
+        String sur = "Surit";
+        sur = "New SUrit";
+        System.out.println(sur);
+    }
+
+}
